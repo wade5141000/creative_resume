5ec227b6ecc355663b350483017b71b973230479
Wade <wade5141000@outlook.com>
2020-08-01 18:36:58 +0800

    dashboard controller layout調整，增加sidebar topbar

---
+++ src/main/java/com/sedia/my_course/web/DashboardController.java
@@ -0,0 +1,20 @@
+package com.sedia.my_course.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/dashboard")
+public class DashboardController {
+
+
+	@GetMapping
+	public String dashboardPage(Model model){
+
+		return "dashboard/dashboard";
+	}
+
+
+}
